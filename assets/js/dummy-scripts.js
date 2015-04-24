17868667c8b6fc5cbfc21ca822b01ac71abfa717
Sigurður Gísli Sigurðsson <gilli@Silver.local>
2015-04-24 19:10:57 +0000

    add dummy scripts

--- assets/js/dummy-scripts.js
+++ assets/js/dummy-scripts.js
@@ -1,9 +1,22 @@
 var rsWidget = document.getElementById("rs-widget");
+var rsInitial = document.getElementsByClassName("rs-box-initial");
+var rsChooseButton = document.getElementById("rs-choose-rs");
 
-if (rsWidget.classList.contains("rs-state-initial")) {
-  console.log("widget is in initial state");
-  rsWidget.onclick = function(e) {
-    console.log("clicked " + rsWidget.classList.remove("rs-state-initial"));
-    rsWidget.classList.add("rs-state-choose");
-  };
-}
+
+// rsWidget.onclick = function(e) {
+//   console.log("clicked inital button"); rsWidget.classList.remove("rs-state-initial");
+//   rsWidget.classList.add("rs-state-choose");
+//   rsWidget.onclick = null;
+// };
+
+rsInitial[0].addEventListener("click", function(e) {
+  console.log("clicked initial button");
+  rsWidget.classList.remove("rs-state-initial");
+  rsWidget.classList.add("rs-state-choose");
+});
+
+rsChooseButton.addEventListener("click", function(e) {
+  console.log("clicked choose rs");
+  rsWidget.classList.remove("rs-state-choose");
+  rsWidget.classList.add("rs-state-connected");
+});
