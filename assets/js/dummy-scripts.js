6396a6a3f420d8a1b5d0761596eb8db4643647c2
Sigurður Gísli Sigurðsson <gilli@Silver.local>
2015-04-27 10:48:42 +0000

    adding disconnect funcionality

--- assets/js/dummy-scripts.js
+++ assets/js/dummy-scripts.js
@@ -1,23 +1,26 @@
 var rsWidget = document.getElementById("rs-widget");
 var rsInitial = document.getElementsByClassName("rs-box-initial");
 var rsChooseButton = document.getElementById("rs-choose-rs");
-var rsDisconnectButton = document.getElementById("rs-disconnect");
+var rsDisconnectButton = document.getElementsByClassName("rs-disconnect");
 
 
-// rsWidget.onclick = function(e) {
-//   console.log("clicked inital button"); rsWidget.classList.remove("rs-state-initial");
-//   rsWidget.classList.add("rs-state-choose");
-//   rsWidget.onclick = null;
-// };
-
+// Initial button
 rsInitial[0].addEventListener("click", function(e) {
   console.log("clicked initial button");
   rsWidget.classList.remove("rs-state-initial");
   rsWidget.classList.add("rs-state-choose");
 });
 
+// Choose provider button
 rsChooseButton.addEventListener("click", function(e) {
   console.log("clicked choose rs");
   rsWidget.classList.remove("rs-state-choose");
   rsWidget.classList.add("rs-state-connected");
+});
+
+// Disconnect button
+rsDisconnectButton[0].addEventListener("click", function(e) {
+  console.log("clicked disconnect button");
+  rsWidget.classList.remove("rs-state-connected");
+  rsWidget.classList.add("rs-state-initial");
 });
