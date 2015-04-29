bccc3fe1617e65879c2045e748673c5b791516f6
Gilli Sigurdsson <gilli@axe.is>
2015-04-29 20:10:16 +0000

    moving scripts to one file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,4 +25,72 @@ function initOnCompleteLoad () {
 }
 
 // TODO
-// Add responsive height to the rs-widget
+// CSS can't animate to responsive height so we need to check the
+// height and add it with JS
+var chooseBox = document.getElementsByClassName("rs-box-choose")[0];
+var chooseBoxHeight = chooseBox.clientHeight;
+// chooseBox.style.height = "0";
+chooseBox.setAttribute("style", "height: 0");
+
+// Also center sign in box
+
+
+
+
+var signInBox = document.getElementsByClassName("rs-box-sign-in")[0];
+var signInBoxHeight = signInBox.clientHeight;
+
+
+
+
+// Button events
+
+
+var rsWidget = document.getElementById("rs-widget");
+var rsInitial = document.getElementsByClassName("rs-box-initial");
+var rsChooseButton = document.getElementsByClassName("rs-big-button");
+var rsDisconnectButton = document.getElementsByClassName("rs-disconnect");
+var rsSignIn = document.getElementsByClassName("rs-submit")
+
+// Initial button
+rsInitial[0].addEventListener("click", function(e) {
+  console.log("clicked initial button");
+  rsWidget.classList.remove("rs-state-initial");
+  rsWidget.classList.add("rs-state-choose");
+
+  chooseBox.setAttribute("style", "height: " + chooseBoxHeight);
+});
+
+// Choose RS button
+rsChooseButton[0].addEventListener("click", function(e) {
+  console.log("clicked RS button");
+  rsWidget.classList.remove("rs-state-choose");
+  rsWidget.classList.add("rs-state-sign-in");
+});
+
+// Sign in button
+rsSignIn[0].addEventListener("click", function(e) {
+  rsWidget.classList.remove("rs-state-sign-in");
+  rsWidget.classList.add("rs-state-connected");
+});
+
+// Choose Dropbox button
+rsChooseButton[1].addEventListener("click", function(e) {
+  console.log("clicked Dropbox button");
+  rsWidget.classList.remove("rs-state-choose");
+  rsWidget.classList.add("rs-state-connected");
+});
+
+// Choose Google drive button
+rsChooseButton[2].addEventListener("click", function(e) {
+  console.log("clicked Google drive Button");
+  rsWidget.classList.remove("rs-state-choose");
+  rsWidget.classList.add("rs-state-connected");
+});
+
+// Disconnect button
+rsDisconnectButton[0].addEventListener("click", function(e) {
+  console.log("clicked disconnect button");
+  rsWidget.classList.remove("rs-state-connected");
+  rsWidget.classList.add("rs-state-initial");
+});
