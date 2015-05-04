3e67d59f9818e349632344e769b49130230b7ee0
Gilli Sigurdsson <gilli@axe.is>
2015-05-04 13:41:00 +0000

    more transitions and close button for mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,29 +3,29 @@
 // so we need to store the height, set it to 0 and use it when we want the animation
 var chooseBox = document.getElementsByClassName("rs-box-choose")[0];
 var chooseBoxHeight = chooseBox.clientHeight;
-// Set the height to zero until the initial button is triggered
+// Set the height to zero until the initial button is clicked
 chooseBox.setAttribute("style", "height: 0");
 
-
-// Also center sign in box
+// Lets make the sign in box the same height as the choose box and center its
+// That way the input should be exactly where the "Choose RS" button was
 var signInBox = document.getElementsByClassName("rs-box-sign-in")[0];
-var signInBoxHeight = signInBox.clientHeight;
 var signInContent = document.getElementsByClassName("rs-sign-in-content")[0];
 var signInContentHeight = signInContent.clientHeight;
-
+// Todo on window resize check height of box and adjust it
 
 // Button events
 var rsWidget = document.getElementById("rs-widget");
-var rsInitial = document.getElementsByClassName("rs-box-initial");
+var rsCloseButton = document.getElementsByClassName("rs-close")[0];
+var rsInitial = document.getElementsByClassName("rs-box-initial")[0];
 var rsChooseButton = document.getElementsByClassName("rs-button-big");
-var rsDisconnectButton = document.getElementsByClassName("rs-disconnect");
-var rsSyncButton = document.getElementsByClassName("rs-sync");
-var rsSignIn = document.getElementsByClassName("rs-submit");
+var rsDisconnectButton = document.getElementsByClassName("rs-disconnect")[0];
+var rsSyncButton = document.getElementsByClassName("rs-sync")[0];
+var rsSignInButton = document.getElementsByClassName("rs-submit")[0];
 var rsConnected = document.getElementsByClassName("rs-box-connected")[0];
 rsConnected.setAttribute("style", "display: none;");
 
 // Initial button
-rsInitial[0].addEventListener("click", function(e) {
+rsInitial.addEventListener("click", function(e) {
   console.log("clicked initial button");
   rsWidget.classList.remove("rs-state-initial");
   rsWidget.classList.add("rs-state-choose");
@@ -48,12 +48,12 @@ rsChooseButton[0].addEventListener("click", function(e) {
 });
 
 // Sign in button
-rsSignIn[0].addEventListener("click", function(e) {
+rsSignInButton.addEventListener("click", function(e) {
   rsWidget.classList.remove("rs-state-sign-in");
   rsWidget.classList.add("rs-state-connected");
 
+  delayFadeIn(rsConnected, 600);
   signInBox.setAttribute("style", "height: 0;");
-  delayConnected();
 });
 
 // Choose Dropbox button
@@ -63,7 +63,7 @@ rsChooseButton[1].addEventListener("click", function(e) {
   rsWidget.classList.add("rs-state-connected");
   chooseBox.setAttribute("style", "height: 0");
 
-  delayConnected();
+  delayFadeIn(rsConnected, 600);
 });
 
 // Choose Google drive button
@@ -73,33 +73,42 @@ rsChooseButton[2].addEventListener("click", function(e) {
   rsWidget.classList.add("rs-state-connected");
   chooseBox.setAttribute("style", "height: 0");
 
-  delayConnected();
+  delayFadeIn(rsConnected, 600);
 });
 
 // Disconnect button
-rsDisconnectButton[0].addEventListener("click", function(e) {
+rsDisconnectButton.addEventListener("click", function(e) {
   console.log("clicked disconnect button");
   rsWidget.classList.remove("rs-state-connected");
   rsWidget.classList.add("rs-state-initial");
 
   fadeOut(rsConnected);
-  fadeIn(rsInitial[0]);
+  delayFadeIn(rsInitial, 300);
 });
 
 // Sync button
-rsSyncButton[0].addEventListener("click", function(e) {
+rsSyncButton.addEventListener("click", function(e) {
   console.log("clicked sync button");
-  rsSyncButton[0].classList.toggle("rs-rotate");
+  rsSyncButton.classList.toggle("rs-rotate");
+});
+
+// Close button
+rsCloseButton.addEventListener("click", function(e) {
+  console.log("clicked close button");
+  rsWidget.classList.remove("rs-state-sign-in");
+  rsWidget.classList.remove("rs-state-choose");
+
+  delayFadeIn(rsInitial, 300);
+  signInBox.setAttribute("style", "height: 0;");
+  chooseBox.setAttribute("style", "height: 0;");
 });
 
 
-// Wait until the choose/sign-in box has scrolled up and the logo is back
-// Then fade the connected screen in.
-function delayConnected() {
+// Delay the fade in until other animatiosn are finished
+function delayFadeIn(element, delayTime) {
   setTimeout(function() {
-    console.log("fade in");
-    fadeIn(rsConnected);
-  }, 600);
+    fadeIn(element);
+  }, delayTime);
 }
 
 // CSS can't fade elements in and out of DOM so we have to do it in JS
@@ -108,22 +117,22 @@ function fadeOut(element) {
   var timer = setInterval(function () {
     if (op <= 0.1){
       clearInterval(timer);
-      element.style.display = 'none';
+      element.style.display = "none";
     }
     element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    element.style.filter = "alpha(opacity=" + op * 100 + ")";
     op -= op * 0.1;
   }, 3);
 }
 function fadeIn(element) {
   var op = 0.1;  // initial opacity
-  element.style.display = 'block';
+  element.style.display = "block";
   var timer = setInterval(function () {
     if (op >= 1){
       clearInterval(timer);
     }
     element.style.opacity = op;
-    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    element.style.filter = "alpha(opacity=" + op * 100 + ")";
     op += op * 0.1;
   }, 3);
 }
