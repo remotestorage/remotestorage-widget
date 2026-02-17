d89255e6d3ec97100c5e8d776ff122b7f28595c9
m5x5 <michaelpeters5647@gmail.com>
2026-02-18 00:14:24 +0100

    add typescript import type definitions

---
+++ src/types/assets.d.ts
@@ -0,0 +1,15 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.html" {
+  const content: string;
+  export default content;
+}
+
